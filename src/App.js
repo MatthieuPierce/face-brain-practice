d60d8ff1258a3cc14257b169042414d434af27c7
Matthieu Pierce <matthieu.pierce@gmail.com>
2020-07-15 11:06:43 -0700

    face-brain user interface and initial API request

--- src/App.js
+++ src/App.js
@@ -1,24 +1,166 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/nav/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imagelinkform/ImageLinkForm.js';
-// import FaceRecognition from '';
+import FaceRecognition from './components/face-recognition/FaceRecognition.js';
+import SignIn from './components/signin/SignIn'
 import 'tachyons';
 import Rank from './components/rank/Rank';
+import Particles from "react-tsparticles";
+import Clarifai from 'clarifai';
 
+const app = new Clarifai.App({
+  apiKey: '3ef20ec22f8a488cb9739634bf657fa4'
+});
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+const particlesOptions = {
+  background: {
+    color: {
+      value: "transparent",
+    },
+  },
+  fpsLimit: 60,
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 6,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
 }
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin'
+    }
+  }
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+caclulateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  console.log(height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+  }
+}
+
+displayFaceBox = (box) => {
+  console.log(box)
+  this.setState({ box: box })
+
+}
+
+onButtonSubmit = () => {
+  console.log('click');
+  this.setState({imageUrl: this.state.input});
+  app.models.predict(
+    Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => this.displayFaceBox(this.caclulateFaceLocation(response)))
+    .catch(err => console.error(err));
+}
+
+onRouteChange = () => {
+  this.setState({route: 'home'});
+}
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          id="tsparticles"
+          options={particlesOptions}
+        />
+      
+        <Navigation onRouteChange={this.onRouteChange}/>
+        { this.state.route === 'signin' 
+          ? <SignIn onRouteChange={this.onRouteChange} />
+          : <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box}/>
+            </div>
+          }
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/imagelinkform/ImageLinkForm.js
+++ src/components/imagelinkform/ImageLinkForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div className='ma4 mt0 white'>
       <p className='f3'>
@@ -9,8 +11,9 @@ const ImageLinkForm = () => {
       </p>
       <div className='center'>
         <div className='pa4 br3 shadow-5 center form'>
-          <input className='f4 pa2 w-70 center' type='text' placeholder="image link here" />
-          <button className='w-30 grow f4 link ph3 pv dib white bg-purple'>Detect</button>
+          <input className='f4 pa2 w-70 center' type='text' placeholder="image link here" onChange={onInputChange} />
+          <button className='w-30 grow f4 link ph3 pv dib white bg-purple'
+          onClick={onButtonSubmit}>Detect</button>
         </div>
       </div>
     </div>
